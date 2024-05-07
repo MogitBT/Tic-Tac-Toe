6776a87afc811e5ddd962b3e950fcde862bb31b4
Mogit B T <btmogit@gmail.com>
2024-05-08 00:42:36 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def layout():
+    for row in range(3):
+        for col in range(3):
+            print("_",end="")
+            if col != 2:
+                print(" | ", end="")
+        print()
+
+layout()
+
