cd2e4905183e43af276a5a723e11a1a3125140e9
Mogit B T <btmogit@gmail.com>
2024-05-09 00:54:36 +0530

    Add the functions for getting the input from user an pc

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 def layout():
     for row in range(3):
         for col in range(3):
@@ -5,3 +7,16 @@ def layout():
             if col != 2:
                 print(" | ", end="")
         print()
+
+def user_input():
+    user_choice = input("Enter the number of your choice (1-9) : ")
+    return int(user_choice)
+
+def pc_choice():
+    pc_choice = random.randint(0,9)
+    return pc_choice
+
+def move():
+    player = user_input()
+    pc = pc_choice()
+    print(pc)
