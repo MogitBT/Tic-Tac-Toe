c551b908fe4e8e3d4e894bb7dff958977f65a720
Mogit B T <btmogit@gmail.com>
2024-05-16 22:23:51 +0530

    Printing user choice in layout

--- main.py
+++ main.py
@@ -18,7 +18,28 @@ class game:
         pc_choice = random.randint(0,9)
         return pc_choice
 
-def move():
-    player = user_input()
-    pc = pc_choice()
-    print(pc)
+    def update(position, player):
+        structure = [['0' for _ in range(3)] for _ in range(3)]
+        count = 1
+        for row in range(3):
+            for col in range(3):
+                if count == position and player == "human":
+                    structure[row][col] = 'X'
+                elif count == position and player == "pc":
+                    structure[row][col] = 'O'
+                print(structure[row][col],end="")
+                count += 1
+                if col!=2:
+                    print(" | ",end="")
+            print("")
+
+
+def main():
+    while True:
+        player_position = game.user_input()
+        player = "human"
+        game.update(player_position,player)
+
+
+if __name__ == "__main__":
+    main()
